7864e6ae9e8b84164eb2e09dcc30adb498f67f76
philippe117 <philippe117@live.fr>
2017-07-04 11:00:24 -0400

    fix package and add movement server

---
+++ include/sara_moveit/move_arm_server.h
@@ -0,0 +1,26 @@
+////
+//// Created by philippe on 02/07/17.
+////
+//
+#ifndef PROJECT_MOVE_ARM_ACTION_SERVER_H
+#define PROJECT_MOVE_ARM_ACTION_SERVER_H
+//
+//
+#include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <geometry_msgs/Pose.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <geometric_shapes/solid_primitive_dims.h>
+//
+//moveit::planning_interface::MoveGroupInterface group;
+//
+//
+//
+//
+////class move_arm_action_server {
+////
+////};
+//
+//
+#endif //PROJECT_MOVE_ARM_ACTION_SERVER_H
